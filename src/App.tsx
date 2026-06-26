576ecd9e854a007ecfe796a648cf354576a1f15d
Sakshamkhadka7 <sakshamcode12@gmail.com>
2026-06-26 22:05:48 +0545

    added home UI

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,12 @@ import React from 'react'
 import { Route, Routes } from 'react-router-dom'
 import Register from './pages/user/Register'
 import Login from './pages/user/Login'
+import Home from './pages/home/Home'
 
 const App = () => {
   return (
     <Routes>
+      <Route path='/' element={<Home/>} />
       <Route path='/register' element={<Register/>} />
       <Route path='/login' element={<Login/>} />
     </Routes>
