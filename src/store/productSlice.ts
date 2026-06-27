e8a91ac46c2d4f7e3a828a98579fb46121862f27
Sakshamkhadka7 <sakshamcode12@gmail.com>
2026-06-27 10:03:57 +0545

    optimize getSingleProduct by checking in a product array

--- src/store/productSlice.ts
+++ src/store/productSlice.ts
@@ -1,13 +1,13 @@
 import { createSlice, type PayloadAction } from "@reduxjs/toolkit";
 import type { IProduct, IProducts } from "../pages/product/types/productType";
 import { Status } from "../globals/types/type";
-import type { AppDispatch } from "./store";
+import type { AppDispatch, RootState } from "./store";
 import axios from "axios";
 
 const initialState: IProducts = {
   products: [],
   status: Status.LOADING,
-  product:null
+  product: null,
 };
 
 const productSlice = createSlice({
@@ -20,13 +20,13 @@ const productSlice = createSlice({
     setStatus(state: IProducts, action: PayloadAction<Status>) {
       state.status = action.payload;
     },
-    setProduct(state:IProducts,action:PayloadAction<IProduct>){
-      state.product=action.payload
-    }
+    setProduct(state: IProducts, action: PayloadAction<IProduct>) {
+      state.product = action.payload;
+    },
   },
 });
 
-export const { setProducts, setStatus ,setProduct } = productSlice.actions;
+export const { setProducts, setStatus, setProduct } = productSlice.actions;
 export default productSlice.reducer;
 
 export function fetchProduct() {
@@ -49,24 +49,36 @@ export function fetchProduct() {
   };
 }
 
-export function singleFetchProduct(id:string) {
-  return async function singleFetchProductThunk(dispatch: AppDispatch) {
-    try {
-      const response = await axios.get(
-        `http://localhost:3000/api/product/getSingleProduct/${id}`,
-      );
-      console.log(response);
-      console.log("Response",response.data.data[0]);
+export function singleFetchProduct(id: string) {
+  return async function singleFetchProductThunk(
+    dispatch: AppDispatch,
+    getState: () => RootState,
+  ) {
+    const store = getState();
+    const productExists = store.products.products.find(
+      (product) => product.productId === id,
+    );
+    if (productExists) {
+      dispatch(setProduct(productExists));
+      dispatch(setStatus(Status.SUCCESS));
+    } else {
+      try {
+        const response = await axios.get(
+          `http://localhost:3000/api/product/getSingleProduct/${id}`,
+        );
+        console.log(response);
+        console.log("Response", response.data.data[0]);
 
-      if (response.status === 200) {
-        dispatch(setStatus(Status.SUCCESS));
-        dispatch(setProduct(response.data.data[0]));
-      } else {
-        dispatch(setStatus(Status.ERROR));
+        if (response.status === 200) {
+          dispatch(setStatus(Status.SUCCESS));
+          dispatch(setProduct(response.data.data[0]));
+        } else {
+          dispatch(setStatus(Status.ERROR));
+        }
+      } catch (error) {
+        console.log("Error occured at fetchdProducts", error);
+        setStatus(Status.ERROR);
       }
-    } catch (error) {
-      console.log("Error occured at fetchdProducts", error);
-      setStatus(Status.ERROR);
     }
   };
 }
--- src/store/store.ts
+++ src/store/store.ts
@@ -1,15 +1,15 @@
 import { configureStore } from "@reduxjs/toolkit";
-import authSlice from "./authSlice"
-import productSlice from "./productSlice"
+import authSlice from "./authSlice";
+import productSlice from "./productSlice";
 
-const store= configureStore({
-    reducer:{
-     auth:authSlice,
-     products:productSlice
-    }
-})
+const store = configureStore({
+  reducer: {
+    auth: authSlice,
+    products: productSlice,
+  },
+});
 
-export default store
+export default store;
 
-export type AppDispatch=typeof store.dispatch
-export type RootState=ReturnType<typeof store.getState>
+export type AppDispatch = typeof store.dispatch;
+export type RootState = ReturnType<typeof store.getState>;
