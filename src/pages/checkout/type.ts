f958efebca6c5bc07463b2e82ce916c96e64e573
Sakshamkhadka7 <sakshamcode12@gmail.com>
2026-06-28 16:22:09 +0545

    added a khaltiUrl in a checkout type

--- src/pages/checkout/type.ts
+++ src/pages/checkout/type.ts
@@ -7,6 +7,7 @@ export interface IProduct {
 
 export interface IOrderItems extends IProduct {
   orderId: string;
+  khaltiUrl:string
 }
 
 export interface IOrder {
--- src/store/checkoutSlice.ts
+++ src/store/checkoutSlice.ts
@@ -4,38 +4,41 @@ import { Status } from "../globals/types/type";
 import type { AppDispatch } from "./store";
 import { APIWITHTOKEN } from "../http";
 
-const initialState:IOrder={
-  items:[],
-  status:Status.LOADING
-}
+const initialState: IOrder = {
+  items: [],
+  status: Status.LOADING,
+};
 
-
-const orderSlice=createSlice({
-    name:"orders",
-    initialState,
-    reducers:{
-        setItems(state:IOrder,action:PayloadAction<IOrderItems[]>){
-            state.items=action.payload
-        },
-        setStatus(state:IOrder,action:PayloadAction<Status>){
-            state.status=action.payload
-        }
-    }
-})
-
+const orderSlice = createSlice({
+  name: "orders",
+  initialState,
+  reducers: {
+    setItems(state: IOrder, action: PayloadAction<IOrderItems[]>) {
+      state.items = action.payload;
+    },
+    setStatus(state: IOrder, action: PayloadAction<Status>) {
+      state.status = action.payload;
+    },
+  },
+});
 
 export default orderSlice.reducer;
-export const {setItems,setStatus}=orderSlice.actions
+export const { setItems, setStatus } = orderSlice.actions;
 
-export function orderItem(data:IData){
- return async function orderItemThunk(dispatch:AppDispatch){
-       try {
-        const response=await APIWITHTOKEN.post("/createorder",{
-            
-        })
+export function orderItem(data: IData) {
+  return async function orderItemThunk(dispatch: AppDispatch) {
+    try {
+      const response = await APIWITHTOKEN.post("/createorder", {
+        data,
+      });
+      if (response.status === 200) {
+        dispatch(setItems(response.data.data));
+      } else {
+        dispatch(setStatus(Status.ERROR));
+      }
     } catch (error) {
-        console.log("Error occured at orderItem",error);
-        dispatch(setStatus(Status.ERROR))   
+      console.log("Error occured at orderItem", error);
+      dispatch(setStatus(Status.ERROR));
     }
- }
+  };
 }
